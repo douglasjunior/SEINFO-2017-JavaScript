29a93584ceb324b51612e2ff60643751f7c8507f
Edvaldo Szymonek <edvaldoszy@gmail.com>
2017-08-28 00:27:30 -0300

    Coisa boba :D

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-    res.render('index', { title: 'Express' });
+router.get('/', function (request, response, next) {
+    response.redirect("https://github.com/douglasjunior/SEINFO-2017-JavaScript");
 });
 
 module.exports = router;
