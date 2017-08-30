b5937927d29f572f5e5cc8031b0de4eaca446c01
Edvaldo Szymonek <edvaldoszy@gmail.com>
2017-08-30 00:49:47 -0300

    Removido modelo de usuários

--- backend/models/Tarefa.js
+++ backend/models/Tarefa.js
@@ -9,14 +9,6 @@ module.exports = function (sequelize, DataTypes) {
     });
 
     Tarefa.associate = function (models) {
-        // Using additional options like CASCADE etc for demonstration
-        // Can also simply do Task.belongsTo(models.User);
-        Tarefa.belongsTo(models.Usuario, {
-            onDelete: "NO ACTION",
-            foreignKey: {
-                allowNull: true
-            }
-        });
     }
 
     return Tarefa;
